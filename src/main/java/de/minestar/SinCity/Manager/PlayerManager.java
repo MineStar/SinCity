293f1c5ca064b8019fca0e2e067be37ed77db432
GeMoschen <x-chris-x@arcor.de>
2012-03-01 11:16:39 +0100

    - we do NOT iterate over all players...

--- src/main/java/de/minestar/SinCity/Manager/PlayerManager.java
+++ src/main/java/de/minestar/SinCity/Manager/PlayerManager.java
@@ -1,10 +1,13 @@
 package de.minestar.SinCity.Manager;
 
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import de.minestar.SinCity.Core;
 import de.minestar.SinCity.Units.SinCityPlayer;
 import de.minestar.minestarlibrary.utils.ChatUtils;
 
@@ -23,14 +26,17 @@ public class PlayerManager {
     // /////////////////////////////////////////////
 
     public void sendToOps(String message) {
-        Player[] players = Bukkit.getOnlinePlayers();
-        for (Player player : players) {
-            if (player.isOp()) {
-                ChatUtils.writeError(player, "[ SinCity ]", message);
-            }
+        Set<OfflinePlayer> players = Bukkit.getOperators();
+        Player player;
+        for (OfflinePlayer offPlayer : players) {
+            // ONLY ONLINE
+            if (!offPlayer.isOnline())
+                continue;
+
+            player = offPlayer.getPlayer();
+            ChatUtils.writeError(player, Core.pluginName, message);
         }
     }
-
     // /////////////////////////////////////////////
     //
     // UPDATE ALL
